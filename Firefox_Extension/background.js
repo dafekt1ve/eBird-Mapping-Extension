bdad6e9d0d094f952c99e63a1f4a252cfac6c7e3
Bryan Guarente <bryan.guarente@gmail.com>
2025-04-11 19:26:37 -0600

    Updated Firefox Extension for gradient colorscale.

--- Firefox_Extension/background.js
+++ Firefox_Extension/background.js
@@ -1,5 +1,27 @@
 browser.browserAction.onClicked.addListener((tab) => {
-  browser.tabs.executeScript(tab.id, { file: "leaflet/leaflet.js" });
-  browser.tabs.insertCSS(tab.id, { file: "leaflet/leaflet.css" });
-  browser.tabs.executeScript(tab.id, { file: "map.js" });
+  if (tab.url && tab.url.startsWith("https://ebird.org/alert/")) {
+    browser.tabs.insertCSS(tab.id, { file: "leaflet/leaflet.css" });
+
+    browser.tabs.executeScript(tab.id, { file: "leaflet/leaflet.js" }).then(() => {
+      return browser.tabs.executeScript(tab.id, { file: "d3/d3.v7.min.js" });
+    }).then(() => {
+      return browser.tabs.executeScript(tab.id, { file: "map.js" });
+    }).catch((err) => {
+      console.error("Script injection failed:", err);
+      browser.notifications.create("Error", {
+        type: "basic",
+        iconUrl: "icon.png",
+        title: "Warning",
+        message: "Failed to inject required scripts. Check the console."
+      });
+    });
+  } else {
+    console.log("This extension only works on eBird alert pages.");
+    browser.notifications.create("Error", {
+      type: "basic",
+      iconUrl: "icon.png",
+      title: "Warning",
+      message: "This extension can only be used on eBird Alert pages."
+    });
+  }
 });
